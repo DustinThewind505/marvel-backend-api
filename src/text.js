22b5f2e54ca0ac5df359d29aea4cb044b3be5920
Dustin G <42304513+DustinThewind505@users.noreply.github.com>
2020-02-01 21:18:31 -0700

    initial commit

---
+++ src/text.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function Geniesus() {
+  const [i, setI] = useState(false);
+
+  setTimeout(() => {
+    setI(!i);
+  }, 1000);
+
+  if (i) {
+    return (
+      <div>
+        When I<br />
+        <Wrestle />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        When YOU
+        <br />
+        <Wrestle />
+      </div>
+    );
+  }
+}
+
+function Wrestle() {
+  const [fYou, setFYou] = useState(false);
+
+  setTimeout(() => {
+    setFYou(!fYou);
+  }, 1000);
+
+  if (fYou) {
+    return (
+      <div>
+        Love YOU
+        <br />
+        <Bulb />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        Love ME
+        <br />
+        <Bulb />
+      </div>
+    );
+  }
+}
+
+function Bulb() {
+  const [isBulbOn, setIsBulbOn] = useState(false);
+
+  setTimeout(() => {
+    setIsBulbOn(!isBulbOn);
+  }, 1000);
+
+  return <div>The lighbulb is {isBulbOn ? "ON" : "OFF"}</div>;
+
+  // if (isBulbOn) {
+  //   return (
+  //     <div>
+  //       The lightbulb is ON
+  //       <br />
+  //       {/* <Animorph /> */}
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       The lightbulb is OFF
+  //       <br />
+  //       {/* <Animorph /> */}
+  //     </div>
+  //   );
+  // }
+}
+
+// function Animorph() {
+//   const [animorph, setAnimorph] = useState(false);
+
+//   setTimeout(() => {
+//     setAnimorph(!animorph);
+//   }, 1000);
+
+//   if (animorph) {
+//     return (
+//       <div>
+//         And the animorph tranforms into a CAMEL<br />
+//         <Animorph />
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         And the animorph tranforms into a PEACOCK<br />
+//         <Animorph />
+//       </div>
+//     );
+//   }
+// }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Geniesus />, rootElement);
+
+// function Next() {
+//   const [anything, setAnything] = useState(false);
+
+//   if (anything) {
+//     return <div>I can be THIS</div>;
+//   } else {
+//     return <div>I can be THAT</div>;
+//   }
+// }
