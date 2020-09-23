47c04b6f27971a1da9c721115bac999cf6021e52
unknown <Dustin.Guillen14@yahoo.com>
2020-09-22 19:11:24 -0600

    day 2

--- Graphs/Graphs/projects/ancestor/ancestor.py
+++ Graphs/Graphs/projects/ancestor/ancestor.py
@@ -1,3 +1,23 @@
+from utils import Queue
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    q = Queue()
+    q.enqueue([starting_node])
+
+    while q.size() > 0:
+        current_path = q.dequeue()
+        new_path = []
+        changed = False
+
+        for v in current_path:
+            for a in ancestors:
+                if a[1] == v:
+                    new_path.append(a[0])
+                    changed = True
+                    q.enqueue(new_path)
+        if changed is False:
+            if current_path[0] == starting_node:
+                return -1
+            else:
+                return current_path[0]
